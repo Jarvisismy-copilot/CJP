002e6ba1df1a70966e2b21b7e28afce439b72dbe
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-07-26 19:10:51 -0600

    nav bar

--- CJ's Portfolio/src/components/Navbar.jsx
+++ CJ's Portfolio/src/components/Navbar.jsx
@@ -1,1 +1,22 @@
-﻿
+﻿import React from 'react'
+
+const Navbar = () => {
+return (
+    <div>
+        <div>
+            <div>Hi</div>
+            <div>
+                <a href='#home'>Home</a>
+                <a href='#Portfolio'>Portfolio</a>
+                <a href='#Contact'>Contact</a>
+                <a href='#Resume'>Resume</a>
+       
+            </div>
+        </div>
+    </div>
+
+    )
+}
+
+
+export default Navbar
