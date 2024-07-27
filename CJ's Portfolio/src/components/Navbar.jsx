abec3d5f3c86893fa355071a138c40615a2e176b
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-07-26 20:13:51 -0600

    navbar

--- CJ's Portfolio/src/components/Navbar.jsx
+++ CJ's Portfolio/src/components/Navbar.jsx
@@ -1,22 +1,33 @@
-﻿import React from 'react'
+﻿import React from "react";
 
 const Navbar = () => {
-return (
-    <div>
-        <div>
-            <div>Chelsea Jarvis</div>
-            <div>
-                <a href='#home'>Home</a>
-                <a href='#home'>Portfolio</a>
-                <a href='#home'>Contact</a>
-                <a href='#home'>Resume</a>
-            </div>
-            <button>Connect Me</button>
+  return (
+    <nav className="bg-black text-white px-8 md:px-16 1g:px-24">
+      <div className="container py-2 flex justify-center md:justify-between items-center">
+        <div className="text-2xl font-bold hidden md:inline">
+          Chelsea Jarvis
         </div>
-    </div>
+        <div className="space-x-6">
+          <a href="#home" className="hover:text-gray-400">
+            Home
+          </a>
+          <a href="#about" className="hover:text-gray-400">
+            About Me
+          </a>
+          <a href="#projects" className="hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#resume" className="hover:text-gray-400">
+            Resume
+          </a>
+          <a href="#contact" className="hover:text-gray-400">
+            Contact
+          </a>
+        </div>
+        <button className='bg-gradient-to-r from-greeen-400 to-blue-500 text-white hidden md:inline transform transitiion-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me </button>
+      </div>
+    </nav>
+  );
+};
 
-    )
-}
-
-
-export default Navbar
+export default Navbar;
