478a0d1ca5012d391496101c957f8f803dfe0aab
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-08-09 07:09:52 -0600

    added id titled services

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,10 +1,42 @@
 ﻿import React from "react";
 
+const services = [
+
+    {
+        id: 1, 
+        title: ""
+        description: "",
+    }, 
+
+    {
+        id: 2, 
+        title: ""
+        description: "",
+    }, 
+
+    {
+        id: 3, 
+        title: ""
+        description: "",
+    }, 
+
+    {
+        id: 4, 
+        title: ""
+        description: "",
+    }, 
+
+
+
+]
 const Service = () => {
   return (
     <div className="bg-black text-white py-20" id="about">
       <div className="container mx-auto px-8 md:px-16 1g:px-24">
         <h2 className="text-4x1 font-bold text-center mb-12">About Me</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:gride-cols-3 gap-8">
+
+        </div>
       </div>
     </div>
   );
